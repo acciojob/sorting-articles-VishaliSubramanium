45124aa5a580e4e48325d93547600f8217709a09
VishaliSubramanium <43952038+VishaliSubramanium@users.noreply.github.com>
2026-06-26 17:30:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const bands = [
+    'The Plot in You',
+    'The Devil Wears Prada',
+    'Pierce the Veil',
+    'Norma Jean',
+    'The Bled',
+    'Say Anything',
+    'The Midway State',
+    'We Came as Romans',
+    'Counterparts',
+    'Oh, Sleeper',
+    'A Skylit Drive',
+    'Anywhere But Here',
+    'An Old Dog'
+];
+
+function strip(name) {
+    return name.replace(/^(a |an |the )/i, "");
+}
+
+bands.sort(function(a, b) {
+    return strip(a).localeCompare(strip(b));
+});
+
+const ul = document.getElementById("band");
+
+bands.forEach(function(band) {
+    const li = document.createElement("li");
+    li.textContent = band;
+    ul.appendChild(li);
+});
